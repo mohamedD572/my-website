63d403e2aa84a32790340bcc9b423d2ce4233a05
mohamedD572 <a77a.ko7a@gmail.com>
2026-07-27 13:35:28 +0300

    Initialize Firebase app and authentication

--- firebase.js
+++ firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCDKdgCghOBxP68OgdihbL9brrG-y7Ay4E",
+  authDomain: "days-d0a15.firebaseapp.com",
+  projectId: "days-d0a15",
+  storageBucket: "days-d0a15.firebasestorage.app",
+  messagingSenderId: "982589362978",
+  appId: "1:982589362978:web:78f345551a8336c01dcb7b"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
